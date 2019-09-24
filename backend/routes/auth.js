5c79fee8c34390dfc63891ee7012394fcfb29794
mabirizi <mabiriziad@gmail.com>
2019-09-24 18:14:21 +0300

    adding user login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,11 +14,35 @@ router.post("/login", (req, res, next) => {
   const pw = req.body.password;
   // Check if user login is valid
   // If yes, create token and return it to client
-  const token = createToken();
+
+  db.getDb()
+    .collection("users")
+    .findOne({ email: email })
+    .then(userDoc => {
+      return bcrypt.compare(pw, userDoc.password);
+    })
+    .then(result => {
+      if (!result){
+        throw Error();
+      }
+      const token = createToken();
+      res
+        .status(200)
+        .json({ message: "Authentication suceeded.", token: token });
+    })
+    .catch(err => {
+      console.log(err);
+
+      res.status(401).json({
+        message: "Authentication failed, invalid username or password."
+      });
+    });
+
+  // const token = createToken();
   // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: "Authentication failed, invalid username or password." });
+  // res
+  //   .status(401)
+  //   .json({ message: "Authentication failed, invalid username or password." });
 });
 
 router.post("/signup", (req, res, next) => {
@@ -37,9 +61,7 @@ router.post("/signup", (req, res, next) => {
         .then(result => {
           console.log(result);
           const token = createToken();
-          res
-            .status(201)
-            .json({ token: token, user: { email: email } });
+          res.status(201).json({ token: token, user: { email: email } });
         })
         .catch(err => {
           console.log(err);
