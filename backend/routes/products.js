06d7a454e8ca21354453f9c69a1443c06706276e
mabirizi <mabiriziad@gmail.com>
2019-09-24 17:13:20 +0300

    accessing  a single product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,6 +4,7 @@ const mongodb = require("mongodb");
 const db = require('../db');
 
 const Decimal128 = mongodb.Decimal128;
+const ObjectId = mongodb.ObjectId;
 
 
 
@@ -92,8 +93,18 @@ router.get("/", (req, res, next) => {
 
 // Get single product
 router.get("/:id", (req, res, next) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
+  db.getDb()
+  .collection("products")
+  .findOne({_id: new ObjectId(req.params.id) })
+  .then(productDoc => {
+    productDoc.price = productDoc.price.toString();
+    res.status(200).json(productDoc);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({message: 'An error occured'});
+  });
+  
 });
 
 // Add new product
