d3340918c4d326a605be40327c8921fbd3aa63e4
mabirizi <mabiriziad@gmail.com>
2019-09-24 14:04:01 +0300

    Storing price as a decimal128

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,60 +1,61 @@
-const Router = require('express').Router;
-const mongodb = require('mongodb');
+const Router = require("express").Router;
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+const Decimal128 = mongodb.Decimal128;
 
 const router = Router();
 
 const products = [
   {
-    _id: 'fasdlk1j',
-    name: 'Stylish Backpack',
+    _id: "fasdlk1j",
+    name: "Stylish Backpack",
     description:
-      'A stylish backpack for the modern women or men. It easily fits all your stuff.',
+      "A stylish backpack for the modern women or men. It easily fits all your stuff.",
     price: 79.99,
-    image: 'http://localhost:3100/images/product-backpack.jpg'
+    image: "http://localhost:3100/images/product-backpack.jpg"
   },
   {
-    _id: 'asdgfs1',
-    name: 'Lovely Earrings',
+    _id: "asdgfs1",
+    name: "Lovely Earrings",
     description:
       "How could a man resist these lovely earrings? Right - he couldn't.",
     price: 129.59,
-    image: 'http://localhost:3100/images/product-earrings.jpg'
+    image: "http://localhost:3100/images/product-earrings.jpg"
   },
   {
-    _id: 'askjll13',
-    name: 'Working MacBook',
+    _id: "askjll13",
+    name: "Working MacBook",
     description:
-      'Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!',
+      "Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!",
     price: 1799,
-    image: 'http://localhost:3100/images/product-macbook.jpg'
+    image: "http://localhost:3100/images/product-macbook.jpg"
   },
   {
-    _id: 'sfhjk1lj21',
-    name: 'Red Purse',
-    description: 'A red purse. What is special about? It is red!',
+    _id: "sfhjk1lj21",
+    name: "Red Purse",
+    description: "A red purse. What is special about? It is red!",
     price: 159.89,
-    image: 'http://localhost:3100/images/product-purse.jpg'
+    image: "http://localhost:3100/images/product-purse.jpg"
   },
   {
-    _id: 'lkljlkk11',
-    name: 'A T-Shirt',
+    _id: "lkljlkk11",
+    name: "A T-Shirt",
     description:
-      'Never be naked again! This T-Shirt can soon be yours. If you find that buy button.',
+      "Never be naked again! This T-Shirt can soon be yours. If you find that buy button.",
     price: 39.99,
-    image: 'http://localhost:3100/images/product-shirt.jpg'
+    image: "http://localhost:3100/images/product-shirt.jpg"
   },
   {
-    _id: 'sajlfjal11',
-    name: 'Cheap Watch',
-    description: 'It actually is not cheap. But a watch!',
+    _id: "sajlfjal11",
+    name: "Cheap Watch",
+    description: "It actually is not cheap. But a watch!",
     price: 299.99,
-    image: 'http://localhost:3100/images/product-watch.jpg'
+    image: "http://localhost:3100/images/product-watch.jpg"
   }
 ];
 
 // Get list of products products
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // Return a list of dummy products
   // Later, this data will be fetched from MongoDB
   const queryPage = req.query.page;
@@ -70,35 +71,51 @@ router.get('/', (req, res, next) => {
 });
 
 // Get single product
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   const product = products.find(p => p._id === req.params.id);
   res.json(product);
 });
 
 // Add new product
 // Requires logged in user
-router.post('', (req, res, next) => {
+router.post("", (req, res, next) => {
   const newProduct = {
     name: req.body.name,
     description: req.body.description,
-    price: parseFloat(req.body.price), // store this as 128bit decimal in MongoDB
+    price: Decimal128.fromString(req.body.price.toString()), // store this as 128bit decimal in MongoDB
     image: req.body.image
   };
-  const uri = "mongodb+srv://max:Mak@6503@cluster0-fnrcd.mongodb.net/shop?retryWrites=true&w=majority";
-  MongoClient.connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true })
-  .then(client => {
-    client.db().collection('products').insertOne(newProduct);
-    client.close();
-  })
-  .catch(err => {
-    console.log(err);
-  });
-  res.status(201).json({ message: 'Product added', productId: 'DUMMY' });
+  const uri =
+    "mongodb+srv://max:Mak@6503@cluster0-fnrcd.mongodb.net/shop?retryWrites=true&w=majority";
+  MongoClient.connect(
+    uri,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
+    .then(client => {
+      client
+        .db()
+        .collection("products")
+        .insertOne(newProduct)
+        .then(result => {
+          console.log(result);
+          client.close();
+        })
+        .catch(err => {
+          console.log(err);
+          client.close();
+        });
+      client.close();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  res.status(201).json({ message: "Product added", productId: "DUMMY" });
 });
 
 // Edit existing product
 // Requires logged in user
-router.patch('/:id', (req, res, next) => {
+router.patch("/:id", (req, res, next) => {
   const updatedProduct = {
     name: req.body.name,
     description: req.body.description,
@@ -106,13 +123,13 @@ router.patch('/:id', (req, res, next) => {
     image: req.body.image
   };
   console.log(updatedProduct);
-  res.status(200).json({ message: 'Product updated', productId: 'DUMMY' });
+  res.status(200).json({ message: "Product updated", productId: "DUMMY" });
 });
 
 // Delete a product
 // Requires logged in user
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({ message: 'Product deleted' });
+router.delete("/:id", (req, res, next) => {
+  res.status(200).json({ message: "Product deleted" });
 });
 
 module.exports = router;
