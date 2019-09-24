f072ffebf6c90f1700f2924aad3d0487da6e3799
mabirizi <mabiriziad@gmail.com>
2019-09-24 18:00:53 +0300

    signing up users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,15 @@
-const Router = require('express').Router;
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const Router = require("express").Router;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+const db = require("../db");
 const router = Router();
 
 const createToken = () => {
-  return jwt.sign({}, 'secret', { expiresIn: '1h' });
+  return jwt.sign({}, "secret", { expiresIn: "1h" });
 };
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
   // Check if user login is valid
@@ -17,10 +18,10 @@ router.post('/login', (req, res, next) => {
   // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
   res
     .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
+    .json({ message: "Authentication failed, invalid username or password." });
 });
 
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
   // Hash password before storing it in database => Encryption at Rest
@@ -28,15 +29,27 @@ router.post('/signup', (req, res, next) => {
     .hash(pw, 12)
     .then(hashedPW => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: 'dummy@dummy.com' } });
+      // console.log(hashedPW);
+
+      db.getDb()
+        .collection("users")
+        .insertOne({ email: email, password: hasedPW })
+        .then(result => {
+          console.log(result);
+          const token = createToken();
+          res
+            .status(201)
+            .json({ token: token, user: { email: email } });
+        })
+        .catch(err => {
+          console.log(err);
+
+          res.status(500).json({ message: "Creating the User failed" });
+        });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ message: 'Creating the user failed.' });
+      res.status(500).json({ message: "Creating the user failed." });
     });
   // Add user to database
 });
